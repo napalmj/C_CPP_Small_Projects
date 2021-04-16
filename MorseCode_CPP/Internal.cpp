37a6ee1252c9a4f3df530725c0c3afe7e2cfb6e7
Nathaniel Palmer <61099489+napomj@users.noreply.github.com>
2021-04-16 12:09:02 -0700

    Update Internal.cpp

--- MorseCode_CPP/Internal.cpp
+++ MorseCode_CPP/Internal.cpp
@@ -32,7 +32,7 @@ public:
 //reference chart
 //A-0,B-1,C-2,D-3,E-4,F-5,G-6,H-7,I-8,J-9,K-10,L-11,M-12,N-13,O-14,P-15,Q-16,R-17,S-18,T-19,U-20,V-21,W-22,X-23,Y-24,Z-25
 
-//encodes the characters given by user : case sensitive
+//encodes the characters given by user
 void Morse::Internal::encode()
 {
 	int i, j, k = 0, c = inputStrCounter;
